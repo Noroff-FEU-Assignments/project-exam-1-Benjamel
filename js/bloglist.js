24f285c2b6dd580f01846170e133926d16b0dd4f
Benjamel <benjamin.meldal@gmail.com>
2022-11-11 20:24:28 +0100

    bloglist fetch api

--- js/bloglist.js
+++ js/bloglist.js
@@ -1,22 +1,17 @@
-import { message } from "./message.js";
+// import { message } from "./message.js";
 
-const blogList = document.querySelector(".posts-container")
+const blogList = document.querySelector(".posts-container");
+const postHeader = document.querySelector(".posts-header");
 
-const url = "http://exam-blog-site.local/wp-json/wp/v2/posts?per_page=20&_embed";
-
-// const proxyUrl = "https://noroffcors.herokuapp.com/"
-
-// const newUrl = proxyUrl + url;
+const url = "https://www.benjaminmeldal.com/wp-json/wp/v2/posts?per_page=20&_embed";
 
 async function getPosts() {
     try {
         const response = await fetch(url);
         const getResults = await response.json();
-        blogList.innerHTML = "";
-
+    
         console.log(getResults);
         createHTML(getResults);
-
     }
 
     catch (error) {
@@ -27,15 +22,26 @@ async function getPosts() {
 
 getPosts();
 
-function createHTML(blogPosts) {
-    for (let i = 3; i < blogPosts.length; i++) {
-        if (i === 15) {
-            break
-        }
+function createHTML(getResults) {
 
-        blogList.innerHTML +=
-            `<div>
-            <h1>${headerContent.title.rendered}</h1>
-         </div> `;
+    blogList.innerHTML = "";
+
+    postHeader.innerHTML += 
+                            `<div class="posts-header">
+                                <h1>Blog Posts</h1>       
+                            </div>`;
+
+    for(let i = 3; i < getResults.length; i++) {
+        blogList.innerHTML += 
+                                `<div class="blog-list-container">
+                                    <div class="card-header">
+                                        <img src="${getResults[i]._embedded["wp:featuredmedia"]["0"].source_url}" alt="featured image">
+                                    </div>
+                                        <h2>${getResults[i].title.rendered}</h2>
+                                    <div class="card-body">
+                                        <p>${getResults[i]._embedded.author[0].name}</p>
+                                        <p>${getResults[i].date}</p>
+                                    </div>
+                                </div>`;
     }
-}
+};
