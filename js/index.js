69fcc3ef20a2c6d590da91c0ca984b5772c90a69
Benjamel <benjamin.meldal@gmail.com>
2022-11-23 19:19:41 +0100

    s

--- js/index.js
+++ js/index.js
@@ -1,16 +1,12 @@
 import { message } from "./message.js";
 
-// Carousel Content
+//Slider
 const sliderUrl = "https://www.benjaminmeldal.com/wp-json/wp/v2/posts?per_page=20&_embed";
 const sliderTitle = document.querySelector(".slider-title");
-const sliderContainer = document.querySelector(".carousel-container");
 
 // Featured section
 const featuredImgUrl = "https://www.benjaminmeldal.com/wp-json/wp/v2/posts/122?_embed";
 const featuredImg = document.querySelector(".featured-img");
-
-// Footer
-const footerContainer = document.querySelector(".footer-container")
 
 // Fetch Api
 async function fetchData() {
@@ -22,20 +18,19 @@ async function fetchData() {
         const sliderResult = await sliderResponse.json();
 
         createHTML(featuredResult);
-        sliderContent(sliderResult);
-
+        getSlider(sliderResult);
         console.log(sliderResult);
     }
 
     catch (error) {
         console.log(error, "An error ocurred");
-        sliderContainer.innerHTML = message("error", error)
+        carouselContainer.innerHTML = message("error", error)
     }
 }
 
 fetchData();
 
-function createHTML(headerContent, sliderResult) {
+function createHTML(headerContent) {
     featuredImg.innerHTML +=
         `<div class="featured-content">
                                 <img src="${headerContent._embedded["wp:featuredmedia"]["0"].source_url}" alt="">
@@ -52,28 +47,106 @@ function createHTML(headerContent, sliderResult) {
                             </div>`;
 };
 
+//Fetching images slide url
 
-function sliderContent(sliderResult) {
+function getSlider(getResults) {
 
-    sliderContainer.innerHTML = "";
+    carouselSlide.innerHTML = "";
 
-    for (let i = 3; i < sliderResult.length; i++) {
-        sliderContainer.innerHTML += `<a href="/blogspecific.html?id=${sliderResult[i].id}" class="post-link">
-                                        <div class="post-slider">
-                                            <div class="blog-list-container card-shadow">
-                                                <div class="card-header card-image">
-                                                    <img src="${sliderResult[i]._embedded["wp:featuredmedia"]["0"].source_url}" alt="${sliderResult[i].title.rendered}">
-                                                </div>
-                                                <div class="card-body">
-                                                    <h2>${sliderResult[i].title.rendered}</h2>
-                                                    <hr>
-                                                </div>
-                                                <div class="card-footer">
-                                                    <p>${sliderResult[i]._embedded.author[0].name}</p>
-                                                    <p>${sliderResult[i].date}</p>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </a> `;
+
+
+    for (let i = 2; i < getResults.length; i++) {
+
+        if (i === 5) {
+            break
+        }
+
+        carouselSlide.innerHTML += `
+                        <a class="carousel_images current-slide" href="/blogspecific.html?id=${getResults[i].id}">
+                            <img class="carousel_image" src="${getResults[i]._embedded["wp:featuredmedia"]["0"].source_url}" alt="${getResults[i].title.rendered}">
+                        </a>`;
     }
+}
+
+const carouselContainer = document.querySelector(".carousel-container");
+const carouselSlide = document.querySelector(".carousel_slide");
+const track = document.querySelector(".carousel_track");
+const slides = Array.from(track.children);
+const prevButton = document.querySelector(".carousel_button--left");
+const nextButton = document.querySelector(".carousel_button--right");
+const dotsNav = document.querySelector(".carousel_nav");
+const dots = Array.from(dotsNav.children);
+
+const slideWidth = slides[0].getBoundingClientRect().width;
+//Arrange the slides next to each other
+const setSlidePosition = (slide, index) => {
+    slide.style.left = slideWidth * index + "px";
 };
+slides.forEach(setSlidePosition);
+
+const moveToSlide = (track, currentSlide, targetSlide) => {
+    track.style.transform = "translateX(-" + targetSlide.style.left + ")";
+    currentSlide.classList.remove("current-slide");
+    targetSlide.classList.add("current-slide");
+}
+
+const updateDots = (currentDot, targetDot) => {
+    currentDot.classList.remove("current-slide");
+    targetDot.classList.add("current-slide");
+}
+
+const hideShowArrows = (slides, prevButton, nextButton, targetIndex) => {
+    if (targetIndex === 0) {
+        prevButton.classList.add("is-hidden");
+        nextButton.classList.remove("is-hidden");
+    } else if (targetIndex === slides.length - 1) {
+        prevButton.classList.remove("is-hidden");
+        nextButton.classList.add("is-hidden");
+    } else {
+        prevButton.classList.remove("is-hidden");
+        nextButton.classList.remove("is-hidden");
+    }
+}
+
+//When i click left, move slides to the left
+prevButton.addEventListener("click", event => {
+    const currentSlide = track.querySelector(".current-slide");
+    const prevSlide = currentSlide.previousElementSibling;
+    const currentDot = dotsNav.querySelector(".current-slide")
+    const prevDot = currentDot.previousElementSibling;
+    const prevIndex = slides.findIndex(slide => slide === prevSlide);
+
+    moveToSlide(track, currentSlide, prevSlide);
+    updateDots(currentDot, prevDot);
+    hideShowArrows(slides, prevButton, nextButton, prevIndex);
+
+});
+
+//When i click right, move slides to the right
+nextButton.addEventListener("click", event => {
+    const currentSlide = track.querySelector(".current-slide");
+    const nextSlide = currentSlide.nextElementSibling;
+    const currentDot = dotsNav.querySelector(".current-slide")
+    const nextDot = currentDot.nextElementSibling;
+    const nextIndex = slides.findIndex(slide => slide === nextSlide);
+
+    moveToSlide(track, currentSlide, nextSlide);
+    updateDots(currentDot, nextDot);
+    hideShowArrows(slides, prevButton, nextButton, nextIndex);
+});
+
+//When i click the nav indicators, move to that slide
+dotsNav.addEventListener("click", event => {
+    const targetDot = event.target.closest("button");
+
+    if (!targetDot) return;
+
+    const currentSlide = track.querySelector(".current-slide");
+    const currentDot = dotsNav.querySelector(".current-slide");
+    const targetIndex = dots.findIndex(dot => dot === targetDot);
+    const targetSlide = slides[targetIndex];
+
+    moveToSlide(track, currentSlide, targetSlide);
+    updateDots(currentDot, targetDot);
+    hideShowArrows(slides, prevButton, nextButton, targetIndex);
+})
